a98bc5d2cad8f9b81018949710ee499de428180d
jose142312 <josephtheracer7@gmail.com>
2024-10-09 19:14:15 -0600

    Add files via upload

---
+++ portafolio.o/src/main/java/com/mycompany/portafolio/o/PortafolioO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.portafolio.o;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class PortafolioO {
+    
+   System.out,print("igrese la cantidad de empleados");
+   numempleados = intempleadosint
+   
+   system.out.print("ingrese la cantidad del salario del los empleados");
+    
+ 
+    Msi = 0,925
+    ivm =0.0508
+    empleados = JOptionPane,showInputDialog("ingrese la cantidad de empleados ");
+    stringsalario = ("ingrese la cantidade ");
+    
+    for (int salarios = 0; i = empleados; salarios 1++  ){
+    system.out.ptint("")
+}
+}
